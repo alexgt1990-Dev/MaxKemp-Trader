8b6eafaee7dc69c343f5e7c63fdf821dfd7b5f63
alexgt1990-Dev <alexgt1990@gmail.com>
2026-07-06 12:33:10 -0400

    Update trend.py

--- indicators/trend.py
+++ indicators/trend.py
@@ -1,1 +1,7 @@
-
+def add_trend_indicators(data):
+    data["SMA20"] = data["Close"].rolling(20).mean()
+    data["SMA50"] = data["Close"].rolling(50).mean()
+    data["SMA200"] = data["Close"].rolling(200).mean()
+    data["EMA9"] = data["Close"].ewm(span=9, adjust=False).mean()
+    data["EMA21"] = data["Close"].ewm(span=21, adjust=False).mean()
+    return data
