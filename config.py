f0e4789f9edf03585345a9c9d76b5357f8eb2fe9
alexgt1990-Dev <alexgt1990@gmail.com>
2026-07-06 14:20:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 APP_NAME = "MaxKemp Trader Pro"
 
 DEFAULT_TICKERS = [
-    "SPY", "QQQ", "NVDA", "TSLA", "AAPL", "MSFT", "AMD", "META", "AMZN", "GOOGL"
+    "SPY", "QQQ", "IWM", "DIA",
+    "NVDA", "AMD", "TSLA", "AAPL", "MSFT", "META",
+    "AMZN", "GOOGL", "NFLX", "AVGO", "PLTR",
+    "COIN", "MSTR", "SMCI", "ARM", "MU",
+    "BA", "JPM", "XOM", "CVX", "UNH",
+    "COST", "WMT", "NKE", "DIS", "PYPL"
 ]
